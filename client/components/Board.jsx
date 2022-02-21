80f82e56ddc853db9b2a6bf930fdb1323cdf2051
sasymon <scottasymon@gmail.com>
2022-02-21 20:36:35 +1300

    newboard logic

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -16,134 +16,138 @@ export default function Board () {
   const width = 8
   const candyColors = [blueCandy, redCandy, greenCandy, yellowCandy, orangeCandy, purpleCandy]
 
-  function checkforColFour () {
-    for (let i = 0; i < (width * (width - 3)); i++) {
-      const colOfFour = [i, i + width, i + (width * 2), i + (width * 3)]
-      const colorCheck = colorArr[i]
+  // function checkforColFour () {
+  //   for (let i = 0; i < (width * (width - 3)); i++) {
+  //     const colOfFour = [i, i + width, i + (width * 2), i + (width * 3)]
+  //     const colorCheck = colorArr[i]
 
-      if (colOfFour.every(item => colorArr[item] === colorCheck)) {
-        (colOfFour.forEach(item => colorArr[item] = blank))
-        return true
-      }
-    }
-  }
+  //     if (colOfFour.every(item => colorArr[item] === colorCheck)) {
+  //       (colOfFour.forEach(item => colorArr[item] = blank))
+  //       return true
+  //     }
+  //   }
+  // }
 
-  function checkforColThree () {
-    for (let i = 0; i < (width * (width - 2)); i++) {
-      const colOfThree = [i, i + width, i + (width * 2)]
-      const colorCheck = colorArr[i]
+  // function checkforColThree () {
+  //   for (let i = 0; i < (width * (width - 2)); i++) {
+  //     const colOfThree = [i, i + width, i + (width * 2)]
+  //     const colorCheck = colorArr[i]
 
-      if (colOfThree.every(item => colorArr[item] === colorCheck)) {
-        (colOfThree.forEach(item => colorArr[item] = blank))
-        return true
-      }
-    }
-  }
+  //     if (colOfThree.every(item => colorArr[item] === colorCheck)) {
+  //       (colOfThree.forEach(item => colorArr[item] = blank))
+  //       return true
+  //     }
+  //   }
+  // }
 
-  function checkforRowFour () {
-    for (let i = 0; i < (width * width); i++) {
-      const rowOfFOur = [i, i + 1, i + 2, i + 3]
-      const colorCheck = colorArr[i]
-      const skipCheckFour = []
-      if ((i + 1) % width === 0 || (i + 2) % width === 0 || (i + 3) % width === 0) {
-        skipCheckFour.push(i)
-      }
-      if (skipCheckFour.includes(i) === false) {
-        if (rowOfFOur.every(item => colorArr[item] === colorCheck)) {
-          (rowOfFOur.forEach(item => colorArr[item] = blank))
-          return true
-        }
-      }
-    }
-  }
+  // function checkforRowFour () {
+  //   for (let i = 0; i < (width * width); i++) {
+  //     const rowOfFOur = [i, i + 1, i + 2, i + 3]
+  //     const colorCheck = colorArr[i]
+  //     const skipCheckFour = []
+  //     if ((i + 1) % width === 0 || (i + 2) % width === 0 || (i + 3) % width === 0) {
+  //       skipCheckFour.push(i)
+  //     }
+  //     if (skipCheckFour.includes(i) === false) {
+  //       if (rowOfFOur.every(item => colorArr[item] === colorCheck)) {
+  //         (rowOfFOur.forEach(item => colorArr[item] = blank))
+  //         return true
+  //       }
+  //     }
+  //   }
+  // }
 
-  function checkforRowThree () {
-    for (let i = 0; i < (width * width); i++) {
-      const rowOfThree = [i, i + 1, i + 2]
-      const colorCheck = colorArr[i]
-      const skipCheck = []
-      if ((i + 1) % width === 0 || (i + 2) % width === 0) {
-        skipCheck.push(i)
-      }
-      if (skipCheck.includes(i) === false) {
-        if (rowOfThree.every(item => colorArr[item] === colorCheck)) {
-          (rowOfThree.forEach(item => colorArr[item] = blank))
-          return true
-        }
-      }
-    }
-  }
+  // function checkforRowThree () {
+  //   for (let i = 0; i < (width * width); i++) {
+  //     const rowOfThree = [i, i + 1, i + 2]
+  //     const colorCheck = colorArr[i]
+  //     const skipCheck = []
+  //     if ((i + 1) % width === 0 || (i + 2) % width === 0) {
+  //       skipCheck.push(i)
+  //     }
+  //     if (skipCheck.includes(i) === false) {
+  //       if (rowOfThree.every(item => colorArr[item] === colorCheck)) {
+  //         (rowOfThree.forEach(item => colorArr[item] = blank))
+  //         return true
+  //       }
+  //     }
+  //   }
+  // }
 
-  function newItemsToEmptySpace () {
-    for (let i = 0; i < width * (width - 1); i++) {
-      const firstRow = Array.apply(null, Array(width)).map((x, i) => { return i })
-      const checkFirstRow = firstRow.includes(i)
+  // function newItemsToEmptySpace () {
+  //   for (let i = 0; i < width * (width - 1); i++) {
+  //     const firstRow = Array.apply(null, Array(width)).map((x, i) => { return i })
+  //     const checkFirstRow = firstRow.includes(i)
 
-      if (checkFirstRow && colorArr[i] === blank) {
-        colorArr[i] = candyColors[Math.floor(Math.random() * candyColors.length)]
-      }
+  //     if (checkFirstRow && colorArr[i] === blank) {
+  //       colorArr[i] = candyColors[Math.floor(Math.random() * candyColors.length)]
+  //     }
 
-      if ((colorArr[i + width] === blank)) {
-        colorArr[i + width] = colorArr[i]
-        colorArr[i] = blank
-      }
-    }
-  }
+  //     if ((colorArr[i + width] === blank)) {
+  //       colorArr[i + width] = colorArr[i]
+  //       colorArr[i] = blank
+  //     }
+  //   }
+  // }
 
-  function handleOnDragStart (e) {
-    setDraggedItem(e.target)
-  }
+  // function handleOnDragStart (e) {
+  //   setDraggedItem(e.target)
+  // }
 
-  function handleOnDrop (e) {
-    setReplacedItem(e.target)
-  }
+  // function handleOnDrop (e) {
+  //   setReplacedItem(e.target)
+  // }
 
-  function handleOnDragEnd (e) {
-    e.preventDefault()
+  // function handleOnDragEnd (e) {
+  //   e.preventDefault()
 
-    const itemDraggedId = parseInt(draggedItem.getAttribute('data-id'))
-    const itemReplacedId = parseInt(replacedItem.getAttribute('data-id'))
+  //   console.log(document.getElementById('0'))
 
-    const validMoves = [
-      itemDraggedId - width,
-      itemDraggedId + width
-    ]
-    const potentialValidMoves = [
-      itemDraggedId - 1,
-      itemDraggedId + 1
-    ]
-    if (itemDraggedId + 1 % width !== 0 && itemDraggedId % width !== 0) {
-      validMoves.push(...potentialValidMoves)
-    }
-    const validMove = validMoves.includes(itemReplacedId)
+  //   const itemDraggedId = parseInt(draggedItem.getAttribute('id'))
+  //   const itemReplacedId = parseInt(replacedItem.getAttribute('id'))
 
-    if (validMove) {
-      colorArr[itemReplacedId] = draggedItem.getAttribute('src')
-      colorArr[itemDraggedId] = replacedItem.getAttribute('src')
+  //   const validMoves = [
+  //     itemDraggedId - width,
+  //     itemDraggedId + width
+  //   ]
+  //   const potentialValidMoves = [
+  //     itemDraggedId - 1,
+  //     itemDraggedId + 1
+  //   ]
+  //   if (itemDraggedId + 1 % width !== 0 && itemDraggedId % width !== 0) {
+  //     validMoves.push(...potentialValidMoves)
+  //   }
+  //   const validMove = validMoves.includes(itemReplacedId)
 
-      const isARowOfFour = checkforRowFour()
-      const isAColOfFour = checkforColFour()
-      const isARowOfThree = checkforRowThree()
-      const isAColOfThree = checkforColThree()
+  //   if (validMove) {
+  //     colorArr[itemReplacedId] = draggedItem.getAttribute('src')
+  //     colorArr[itemDraggedId] = replacedItem.getAttribute('src')
 
-      if (itemReplacedId && (isARowOfFour || isAColOfFour || isARowOfThree || isAColOfThree)) {
-        setDraggedItem(null)
-        setReplacedItem(null)
-      } else {
-        colorArr[itemReplacedId] = replacedItem.getAttribute('src')
-        colorArr[itemDraggedId] = draggedItem.getAttribute('src')
-        setColorArr([...colorArr])
-      }
-    }
-  }
+  //     const isARowOfFour = checkforRowFour()
+  //     const isAColOfFour = checkforColFour()
+  //     const isARowOfThree = checkforRowThree()
+  //     const isAColOfThree = checkforColThree()
+
+  //     if (itemReplacedId && (isARowOfFour || isAColOfFour || isARowOfThree || isAColOfThree)) {
+  //       setDraggedItem(null)
+  //       setReplacedItem(null)
+  //     } else {
+  //       colorArr[itemReplacedId] = replacedItem.getAttribute('src')
+  //       colorArr[itemDraggedId] = draggedItem.getAttribute('src')
+  //       setColorArr([...colorArr])
+  //     }
+  //   }
+  // }
 
   function createBoard () {
-    const randomColorArr = []
-    for (let i = 0; i < width * width; i++) {
-      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-      randomColorArr.push(randomColor)
+    const newArr = new Array(width).fill(0).map(() => new Array(width).fill(0))
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < width; j++) {
+        newArr[i][j] = candyColors[Math.floor(Math.random() * candyColors.length)]
+      }
     }
-    setColorArr(randomColorArr)
+    console.log(newArr)
+    setColorArr(newArr)
   }
 
   useEffect(() => {
@@ -152,32 +156,35 @@ export default function Board () {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      checkforRowFour()
-      checkforColFour()
-      checkforRowThree()
-      checkforColThree()
-      newItemsToEmptySpace()
+      // checkforRowFour()
+      // checkforColFour()
+      // checkforRowThree()
+      // checkforColThree()
+      // newItemsToEmptySpace()
       setColorArr([...colorArr])
-    }, 100)
+    }, 1000)
     return () => clearInterval(timer)
-  }, [checkforColFour, checkforRowFour, checkforRowThree, checkforColThree, newItemsToEmptySpace, colorArr])
+  }, [colorArr])
 
   return (
     <div className='Board'>
       <div className='game'>
-        {colorArr.map((candyColor, index) => {
-          return <img key={index}
-            src={candyColor}
-            alt={candyColor}
-            data-id={index}
-            draggable='true'
-            onDragOver={(e) => e.preventDefault()}
-            onDragEnter={(e) => e.preventDefault()}
-            onDragLeave={(e) => e.preventDefault()}
-            onDrop={ handleOnDrop }
-            onDragStart={ handleOnDragStart }
-            onDragEnd={ handleOnDragEnd }
-          />
+        {colorArr.forEach((rowArray, x) => {
+          rowArray.map((candy, y) => {
+            console.log(candy)
+            return <img key={`${x}, ${y}`}
+              src={candy}
+              alt={candy}
+              id={`${x}, ${y}`}
+              draggable='true'
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnter={(e) => e.preventDefault()}
+              onDragLeave={(e) => e.preventDefault()}
+            // onDrop={ handleOnDrop }
+            // onDragStart={ handleOnDragStart }
+            // onDragEnd={ handleOnDragEnd }
+            />
+          })
         })}
       </div>
     </div>
