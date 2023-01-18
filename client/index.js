50ae7dc1d8e3a606d44ecce7d1a87736ac8a05fe
Stuart Kent <35883424+StuartKent17@users.noreply.github.com>
2023-01-18 13:59:09 +1300

    Reverted change to index.js

--- client/index.js
+++ client/index.js
@@ -1,23 +1,11 @@
-import App from './components/App';
-import { shallow } from 'enzyme';
+import React from 'react'
+import { render } from 'react-dom'
 
-describe('59796928', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<App></App>);
-  });
-  describe('#handleSubmit', () => {
-    it('should pass', () => {
-      expect(wrapper.exists()).toBeTruthy();
-      wrapper.find('form').simulate('submit');
-      expect(wrapper.state()).toEqual({ number: 21 });
-    });
-  });
-  describe('#handleMultiply', () => {
-    it('should pass', () => {
-      const comp = wrapper.instance();
-      const actual = comp.handleMultiply(2, 10);
-      expect(actual).toBe(20);
-    });
-  });
-});
+import App from './components/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <App />,
+    document.getElementById('app')
+  )
+})
